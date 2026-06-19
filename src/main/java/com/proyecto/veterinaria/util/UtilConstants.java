8c0bf4c809b0c9b6d8e70a0c5ba11e454a8162cf
Arem021 <aremimorales32@gmail.com>
2026-06-19 09:18:55 -0600

    Agregando archivo de constantes

---
+++ src/main/java/com/proyecto/veterinaria/util/UtilConstants.java
@@ -0,0 +1,18 @@
+package com.proyecto.veterinaria.util;
+
+public class UtilConstants {
+    public static String MSG1 = "Consulta correcta";
+    public static String MSG2 = "Adoptante registro correctamente";
+    public static String MSG3 = "Adoptante actualizado correctamente";
+    public static String MSG4 = "Adoptante eliminado correctamente";
+    public static String MSG5 = "Mascota registro correctamente";
+    public static String MSG6 = "Mascota actualizada correctamente";
+    public static String MSG7 = "Mascota eliminada correctamente";
+    public static String MSG8 = "Chip registro correctamente";
+    public static String MSG9 = "Chip actualizado correctamente";
+    public  static String MSG10 = "Vacuna registro correctamente";
+    public static String MSG11 = "Registro vacuna actualizado";
+    public static String MSG12 = "Actualizacionm de registro vacuna correctio";
+    public static Integer CODENEG = 0;
+    public static Integer CODEPOS = 1;
+}
